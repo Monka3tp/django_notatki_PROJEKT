000263bbbffc592282dd3f06a2ad98e593e1857a
Monika <monika.nowicka5@teb.edu.pl>
2024-05-13 09:34:29 +0200

    kod 13.05 dodanie formularza

--- notatki/models.py
+++ notatki/models.py
@@ -18,7 +18,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notatki_posts')
-
+    text = models.TextField()
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
